973fbe845b12309fe2d40fd99c77ec11cf580cc6
TheSchoolFriday <20046797@myrp.edu.sg>
2021-05-28 14:24:24 +0800

    final commit

--- app/src/main/java/sg/edu/rp/c346/id20046797/demophoneforelderly/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20046797/demophoneforelderly/MainActivity.java
@@ -2,13 +2,48 @@ package sg.edu.rp.c346.id20046797.demophoneforelderly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSon, btnDaughter, btnGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSon = findViewById(R.id.buttonSon);
+        btnDaughter = findViewById(R.id.buttonDaughter);
+        btnGoogle = findViewById(R.id.buttonWebsite);
+
+        btnSon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + 92240336));
+                startActivity(intentCall);
+            }
+        });
+
+        btnDaughter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + 89675543));
+                startActivity(intentCall);
+            }
+        });
+
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentWebsite = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
+                startActivity(intentWebsite);
+            }
+        });
     }
 }
